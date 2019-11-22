c5acf1699fa012058a84e8252578c9015f1cb5eb
A. R. RONY <arrony200@gmail.com>
2019-11-22 23:30:05 +0600

    Add files via upload

---
+++ mobile-menu/assets/js/main.js
@@ -0,0 +1,13 @@
+(function (jQuery) {
+    "use strict";  
+
+
+   /*---------------------
+     Push top menu
+     --------------------- */
+    $("div.contact-info").clone().appendTo(".mainmenu nav");
+    $("div.header-top-right").clone().appendTo(".mainmenu nav");
+	
+	
+	
+})(jQuery);	
